779ee39b686e23dc63dcebe65055ed34e4dc293d
zachmoshe <zachmoshe@gmail.com>
2014-06-25 13:15:43 +0100

    adding setup.py and adding 'pip install .' to travis.yml

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+		name='ranges-merger',
+		version='1.0.0',
+		author='Zach Moshe',
+		author_email='zachmoshe@gmail.com',
+		packages=['ranges_merger'],
+		scripts=[],
+		url='https://github.com/zachmoshe/ranges-merger',
+		license='LICENSE.txt',
+		description='A package for merging together multiple (poetentially hierarchical) range sequences',
+		long_description=open('README.md').read(),
+		install_requires=[
+			],
+		)
+
+
+
+
